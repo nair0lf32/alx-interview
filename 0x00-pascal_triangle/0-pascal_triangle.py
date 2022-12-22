6c70991993397739a3977a6356706ac6936b5e49
nair0lf32 <florianedem@gmail.com>
2022-12-22 11:42:25 +0100

    fix task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,16 +4,21 @@
 Pascal triangle in python
 """
 
+
 def pascal_triangle(n):
+    """
+    Returns a list of list of integers representing
+    the pascal triangle sequence for a given n rows
+    or an empty list if n <= 0
+    """
     row = []
     if (n <= 0):
         return row
     cols = [[1]]
-    for i in range(1,n):
+    for i in range(1, n):
         row = [1]
-        for j in range (1, i):
+        for j in range(1, i):
             row.append(cols[i-1][j-1] + cols[i-1][j])
         row.append(1)
         cols.append(row)
-    return cols    
-
+    return cols
